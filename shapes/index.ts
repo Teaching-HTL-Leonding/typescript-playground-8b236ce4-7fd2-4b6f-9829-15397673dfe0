8b0ea32fb48da9bb2ee77454142a497273038803
Rainer Stropek <rainer@timecockpit.com>
2026-09-18 11:13:40 +0200

    Saved in TypeScript Playground at 2026-09-18T09:13:40.313Z

--- shapes/index.ts
+++ shapes/index.ts
@@ -3,7 +3,15 @@ function setup() {
 //Parameters are width and height
 createCanvas(400, 500);
 //Set background colur("orange");
-//Set a line colur and thickness ("blu")
+//Set a line colur and thickness (=weight)
+stroke ("blue");
+strokeWeight (10);
+//fill colur
+fill("yellow");
+//Draw a line (20, 40, 360, 20);
+//Draw a rectangle
+rect(20, 40, 360, 20 );
+
 
 
 

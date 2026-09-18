20eb6f07b2c34e9bc7460821be486041818005c0
Rainer Stropek <rainer@timecockpit.com>
2026-09-18 10:50:25 +0200

    Saved in TypeScript Playground at 2026-09-18T08:50:24.935Z

---
+++ shapes/index.ts
@@ -0,0 +1,18 @@
+function setup() {
+//crate the canvas ("German Leinwand")
+//Parameters are width and height
+createCanvas(400, 500);
+//Set background colur("orange");
+//Set a line colur and thickness ("blu")
+
+
+
+
+
+
+
+
+
+
+
+}

8da696686151b60702c963bcb0cd2d520268e75a
Rainer Stropek <rainer@timecockpit.com>
2026-09-25 10:59:53 +0200

    Saved in TypeScript Playground at 2026-09-25T08:59:52.548Z

--- house/index.ts
+++ house/index.ts
@@ -1,15 +1,15 @@
 function setup() {
-    createCanvas(600,470);
+    createCanvas(600,500);
     background("blue");
 stroke("Red");
-strokeWeight(500);
+strokeWeight(5);
+fill("red");
 strokeCap(SQUARE);
-line(50,430,350,430);
+rect(20,250,350,250);
 //Draw a triangle
-triangle()
-
-
-
+triangle(175,50,390,250,10,250)
+fill("yellow")
+rect(175,480,50,480)
 
 
 

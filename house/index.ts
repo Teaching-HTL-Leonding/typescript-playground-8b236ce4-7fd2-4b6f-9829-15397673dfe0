7298975e84c32e3f45e3994beede8f35db99bc78
Rainer Stropek <rainer@timecockpit.com>
2026-09-25 11:15:50 +0200

    Saved in TypeScript Playground at 2026-09-25T09:15:50.199Z

--- house/index.ts
+++ house/index.ts
@@ -7,13 +7,17 @@ fill("red");
 strokeCap(SQUARE);
 rect(20,250,350,250);
 //Draw a triangle
-triangle(175,50,390,250,10,250);
+noStroke()
+fill("yellow")
+triangle(180,50,390,250,10,250);
 fill("yellow");
 rect(175,400,50,100);
 fill("brown");
 noStroke();
 rect(450,300,50,200);
-cicle()
+fill("green")
+circle(470,250,100)
+
 
 
 
